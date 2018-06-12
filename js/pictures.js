3f9aa8977046b2ced73ed295f6e1c471d859e2c0
Aleksandr Seymor <godseymor@gmail.com>
2018-06-12 15:05:37 +0500

    Создает дополнительные константы, переименовывает ранее созданные

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var URLS = 25;
-var LIKES = 200;
+var URLS_COUNT = 25;
+var LIKES_COUNT = 200;
+var MIN_LIKES_COUNT = 15;
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -19,14 +20,15 @@ var DESCRIPTIONS = [
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
 ];
+var COMMENTS_COUNT = 2;
 var gallery = [];
 
 // функция получения случайного значения
 var getRandomItem = function (n) {
-  if (n === LIKES) {
+  if (n === LIKES_COUNT) {
     n = Math.floor(Math.random() * n);
-    if (n < 15) {
-      return getRandomItem(LIKES);
+    if (n < MIN_LIKES_COUNT) {
+      return getRandomItem(LIKES_COUNT);
     }
   } else if (typeof (n) === 'object') {
     n = (Math.floor(Math.random() * n.length));
@@ -41,7 +43,7 @@ var generatePhotos = function (images) {
   for (var i = 1; i <= images; i++) {
     gallery.push({
       url: 'photos/' + i + '.jpg',
-      likes: getRandomItem(LIKES),
+      likes: getRandomItem(LIKES_COUNT),
       comments: COMMENTS[getRandomItem(COMMENTS)],
       description: DESCRIPTIONS[getRandomItem(DESCRIPTIONS)]
     });
@@ -51,7 +53,7 @@ var generatePhotos = function (images) {
 
 // функция отрисовки всех изображений
 var createPhotoElements = function (images) {
-  generatePhotos(URLS);
+  generatePhotos(images);
   var pictureTemplate = document.querySelector('#picture').content;
   var photosListElement = document.querySelector('.pictures');
   var fragment = document.createDocumentFragment();
@@ -81,7 +83,7 @@ var createPictureElements = function () {
 
   // небольшая функция для заполнения комментариев под фото случайными значениями
   var fillCommentsContent = function () {
-    for (var j = 1; j <= 2; j++) {
+    for (var j = 1; j <= COMMENTS_COUNT; j++) {
       bigPictureComments.querySelector('.social__comment:nth-child(' + j + ') .social__picture').setAttribute('src', 'img/avatar-' + getRandomItem(MAX_COMENTS_COUNT) + '.svg');
       bigPictureComments.querySelector('.social__comment:nth-child(' + j + ') .social__text').textContent = gallery[j].comments;
     }
@@ -95,5 +97,5 @@ var createPictureElements = function () {
   bigPicture.querySelector('.social__loadmore').classList.add('visually-hidden');
 };
 
-createPhotoElements(URLS);
-createPictureElements(URLS);
+createPhotoElements(URLS_COUNT);
+createPictureElements(URLS_COUNT);
